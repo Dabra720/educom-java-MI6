52be1ef0bdfff9d63efb09d20874efc6917b35df
Daan Braas <daanbraas@gmail.com>
2023-04-12 22:50:34 +0200

    Issue #2 Complete

--- src/main/java/nu/educom/MI6/Agent.java
+++ src/main/java/nu/educom/MI6/Agent.java
@@ -1,64 +1,33 @@
 package nu.educom.MI6;
 
 public class Agent {
-    private int serviceNumber = 1000;
-    private String passPhrase = "For ThE Royal QUEEN";
-    private boolean blackList = false;
+    private final String serviceNumber;
+    private boolean loggedIn = false;
 
-    public Agent(){
-//        this.serviceNumber = 1000;
+    public Agent(String serviceNumber){
+        this.serviceNumber = convertNumber(serviceNumber);
         System.out.println("New agent created, servicenr: " + this.serviceNumber);
     }
 
-    public void setBlackList(boolean bool){
-        this.blackList = bool;
+    public void setLoggedIn(boolean bool){
+        this.loggedIn = bool;
     }
-    public void setServiceNumber(int serNum){
-        this.serviceNumber = serNum;
+    public boolean isLoggedIn(){
+        return this.loggedIn;
     }
 
     public String getServiceNumber(){
-        String serviceNumber = "";
-        if(this.serviceNumber < 100 && this.serviceNumber > 9){
-            serviceNumber = "0" + this.serviceNumber;
-        }else if(this.serviceNumber < 10){
-            serviceNumber = "00" + this.serviceNumber;
-        }else{
-            serviceNumber += this.serviceNumber;
+        return this.serviceNumber;
+    }
+    public String convertNumber(String serviceNumber){
+
+        int serNum = Integer.parseInt(serviceNumber);
+        if(serNum < 100 && serNum > 9){
+            serviceNumber = "0" + serNum;
+        }else if(serNum < 10){
+            serviceNumber = "00" + serNum;
         }
         return serviceNumber;
     }
 
-    public boolean validateNumber(){
-//        System.out.println("<< VALIDATION >>");
-
-        if(this.serviceNumber < 1 || this.serviceNumber > 956){
-//            System.out.println("Number validation: TRUE");
-            return true;
-        } else{
-//            System.out.println("Number validation: FALSE");
-            return false;
-        }
-    }
-
-    public boolean validateSecretPhrase(String input){
-        System.out.println("input: |" + input + "|");
-        System.out.println("pass: |" + this.passPhrase + "|");
-        if(input.equals(this.passPhrase)){
-            System.out.println("TRUE");
-            return true;
-        } else{
-            System.out.println("FALSE");
-            return false;
-        }
-    }
-
-    public boolean isNumeric(String str)
-    {
-        for (char c : str.toCharArray())
-        {
-            if (!Character.isDigit(c)) return false;
-        }
-        return true;
-    }
 }
--- src/main/java/nu/educom/MI6/Main.java
+++ src/main/java/nu/educom/MI6/Main.java
@@ -6,60 +6,159 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    Agent p = new Agent();
-    System.out.println("Hello MI6 Agent");
+      List<String> blackList = new ArrayList<>();
+      List<Agent> Agents = new ArrayList<>();
+      Scanner scanner = new Scanner(System.in);
+      boolean check;
+      do{
+          while(true){
+              System.out.println("Enter your service-number: ");
+              String input = scanner.nextLine();
 
-    Scanner scanner = new Scanner(System.in);
+              if(validateNumber(input)){
+                  input = convertNumber(input);
+                  if(!blackList.contains(input)){
+                      Agents.add(new Agent(input));
+                  }else{
+                      System.out.println("This agent is blacklisted");
+                      continue;
+                  }
+                  break;
+              }
+          }
 
-    do{
-      System.out.println("Please enter your service-number:");
-//      try{
-//        int serviceNumber = scanner.nextInt();
-//        p.setServiceNumber(serviceNumber);
-//      } catch(Exception e){
-//        System.out.println("That is not a valid number, try again.");
-//      }
-      // nextLine gebruikt omdat anders de volgende nextLine niet werkt
-      String input = scanner.nextLine();
-      if(input.length() > 3){
-        System.out.println("Number cannot have more than 3 characters, try again.");
-        continue;
-      }
-      if(p.isNumeric(input)){
-        p.setServiceNumber(Integer.parseInt(input));
-        if(p.validateNumber()){
-          System.out.println("That is not a valid number, try again.");
-        }
-      } else {
-        System.out.println("That is not a number, try again.");
-      }
+//          while(true){
+              System.out.println("Enter the secret passphrase:");
+              Agent p = Agents.get(Agents.size()-1);
+              String input = scanner.nextLine();
+              if(validatePass(input)){
+                  p.setLoggedIn(true);
+                  System.out.println("Welcome Agent " + p.getServiceNumber());
+                  System.out.println("You are now logged in.");
+              } else{
+                  System.out.println("Wrong password, you have been blacklisted.");
 
-    } while(p.validateNumber());
+                  blackList.add(p.getServiceNumber());
+              }
+//              break;
+//          }
+
+          check = true;
+          while(check){
+              System.out.println("Choose your next action:");
+              System.out.println("(1)Log in    (2)Show all logged in members    (3)Show all blacklisted members    (4)EXIT");
+              String action = scanner.nextLine();
+
+              switch (action) {
+                  case "1" ->
+                      //Brings you back to the start
+                          check = false;
+                  case "2" -> {
+                      System.out.println("Logged in MI6 Agents:");
+                      for (Agent Agent : Agents) {
+                          if (Agent.isLoggedIn()) {
+                              System.out.println("Number: " + Agent.getServiceNumber());
+                          }
+                      }
+                  }
+                  case "3" -> {
+                      System.out.println("Blacklisted MI6 Agents:");
+                      for (String Agent : blackList) {
+                          System.out.println("Number: " + Agent);
+                      }
+                  }
+                  default -> System.out.println("Not a valid option.");
+              }
+
+          }
+
+      } while (true);
 
 
-//    List<Agent> Agents = new ArrayList<Agent>(16);
-//    if(isNumeric(input)){
-//      Agents.add(new Agent(input));
-//    }
-
-    System.out.println("Hello agent " + p.getServiceNumber());
-
-
-//    boolean valid = false;
+//    Agent p = new Agent();
 //    do {
-      System.out.println("Please enter the secret phrase:");
-      String phrase = scanner.nextLine();
-      if(p.validateSecretPhrase(phrase)){
-//        valid = true;
-        System.out.println("Welcome back agent " + p.getServiceNumber());
-      } else{
-        p.setBlackList(true);
-        System.out.println("You have been blacklisted, agent " + p.getServiceNumber());
-      }
-//    } while (scanner.hasNextLine());
-
-
+//
+//      System.out.println("Hello MI6 Agent");
+//
+//      Scanner scanner = new Scanner(System.in);
+//
+//      do {
+//        System.out.println("Please enter your service-number:");
+//        // nextLine gebruikt omdat anders de volgende nextLine niet werkt
+//        String input = scanner.nextLine();
+//        if (input.length() > 3) {
+//          System.out.println("Number cannot have more than 3 characters, try again.");
+//          continue;
+//        }
+//        if (p.isNumeric(input)) {
+//          p.setServiceNumber(Integer.parseInt(input));
+//          if (p.validateNumber()) {
+//            System.out.println("That is not a valid number, try again.");
+//          }
+//        } else {
+//          System.out.println("That is not a number, try again.");
+//        }
+//
+//      } while (p.validateNumber());
+//
+//      System.out.println("Hello agent " + p.getServiceNumber());
+//
+//      System.out.println("Please enter the secret phrase:");
+//      String phrase = scanner.nextLine();
+//      if (p.validateSecretPhrase(phrase)) {
+//        System.out.println("Welcome back agent " + p.getServiceNumber());
+//      } else {
+//        p.setBlackList(true);
+//        blackList.add(p.getServiceNumber());
+//        System.out.println("You have been blacklisted, agent " + p.getServiceNumber());
+//      }
+//    } while (blackList.contains(p.getServiceNumber()));
   }
 
+  public static boolean validateNumber(String input){
+      boolean valid = false;
+      while(!valid){
+        if(isNumeric(input)) {
+            if (input.length() > 3) {
+                System.out.println("Number cannot have more than 3 characters, try again.");
+                break;
+            }
+            int serviceNumber = Integer.parseInt(input);
+            if(serviceNumber < 1 || serviceNumber > 956){
+                System.out.println("That is not a valid number, try again.");
+                break;
+            } else{
+                valid = true;
+            }
+        } else{
+            System.out.println("That is not a number, try again.");
+            break;
+        }
+      }
+    return valid;
+  }
 
+  public static boolean validatePass(String input){
+      String secretPhrase = "For ThE Royal QUEEN";
+      return input.equals(secretPhrase);
+
+  }
+    public static String convertNumber(String serviceNumber){
+
+        int serNum = Integer.parseInt(serviceNumber);
+        if(serNum < 100 && serNum > 9){
+            serviceNumber = "0" + serNum;
+        }else if(serNum < 10){
+            serviceNumber = "00" + serNum;
+        }
+        return serviceNumber;
+    }
+    public static boolean isNumeric(String str)
+    {
+        for (char c : str.toCharArray())
+        {
+            if (!Character.isDigit(c)) return false;
+        }
+        return true;
+    }
 }

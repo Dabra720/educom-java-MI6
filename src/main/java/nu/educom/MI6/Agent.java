d9162b486f699ef57c3ea840deaefdea6707b1a4
Daan Braas <daanbraas@gmail.com>
2023-04-12 13:15:41 +0200

    Agent login complete

--- src/main/java/nu/educom/MI6/Agent.java
+++ src/main/java/nu/educom/MI6/Agent.java
@@ -1,31 +1,64 @@
 package nu.educom.MI6;
 
 public class Agent {
-    private int serviceNumber;
-    private String passPhrase;
+    private int serviceNumber = 1000;
+    private String passPhrase = "For ThE Royal QUEEN";
     private boolean blackList = false;
 
-    public Agent(int serNum){
-        serviceNumber=serNum;
+    public Agent(){
+//        this.serviceNumber = 1000;
+        System.out.println("New agent created, servicenr: " + this.serviceNumber);
+    }
+
+    public void setBlackList(boolean bool){
+        this.blackList = bool;
+    }
+    public void setServiceNumber(int serNum){
+        this.serviceNumber = serNum;
     }
 
     public String getServiceNumber(){
         String serviceNumber = "";
         if(this.serviceNumber < 100 && this.serviceNumber > 9){
-            if(this.serviceNumber < 10){
-                serviceNumber="00"+this.serviceNumber;
-            }
-            serviceNumber="0"+this.serviceNumber;
+            serviceNumber = "0" + this.serviceNumber;
+        }else if(this.serviceNumber < 10){
+            serviceNumber = "00" + this.serviceNumber;
+        }else{
+            serviceNumber += this.serviceNumber;
         }
-
         return serviceNumber;
     }
 
-    public boolean validateNumber(int serNum){
-        if(serNum > 0 && serNum < 957){
+    public boolean validateNumber(){
+//        System.out.println("<< VALIDATION >>");
+
+        if(this.serviceNumber < 1 || this.serviceNumber > 956){
+//            System.out.println("Number validation: TRUE");
             return true;
         } else{
+//            System.out.println("Number validation: FALSE");
             return false;
         }
+    }
+
+    public boolean validateSecretPhrase(String input){
+        System.out.println("input: |" + input + "|");
+        System.out.println("pass: |" + this.passPhrase + "|");
+        if(input.equals(this.passPhrase)){
+            System.out.println("TRUE");
+            return true;
+        } else{
+            System.out.println("FALSE");
+            return false;
+        }
+    }
+
+    public boolean isNumeric(String str)
+    {
+        for (char c : str.toCharArray())
+        {
+            if (!Character.isDigit(c)) return false;
+        }
+        return true;
     }
 }
--- src/main/java/nu/educom/MI6/Main.java
+++ src/main/java/nu/educom/MI6/Main.java
@@ -6,32 +6,60 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    Agent p = new Agent();
     System.out.println("Hello MI6 Agent");
-    System.out.println("Please enter your service-number:");
 
     Scanner scanner = new Scanner(System.in);
-//    String input = scanner.next();
 
-    int serviceNumber = scanner.nextInt();
+    do{
+      System.out.println("Please enter your service-number:");
+//      try{
+//        int serviceNumber = scanner.nextInt();
+//        p.setServiceNumber(serviceNumber);
+//      } catch(Exception e){
+//        System.out.println("That is not a valid number, try again.");
+//      }
+      // nextLine gebruikt omdat anders de volgende nextLine niet werkt
+      String input = scanner.nextLine();
+      if(input.length() > 3){
+        System.out.println("Number cannot have more than 3 characters, try again.");
+        continue;
+      }
+      if(p.isNumeric(input)){
+        p.setServiceNumber(Integer.parseInt(input));
+        if(p.validateNumber()){
+          System.out.println("That is not a valid number, try again.");
+        }
+      } else {
+        System.out.println("That is not a number, try again.");
+      }
+
+    } while(p.validateNumber());
+
 
 //    List<Agent> Agents = new ArrayList<Agent>(16);
 //    if(isNumeric(input)){
 //      Agents.add(new Agent(input));
 //    }
-    Agent p = new Agent(serviceNumber);
-
-
 
     System.out.println("Hello agent " + p.getServiceNumber());
 
+
+//    boolean valid = false;
+//    do {
+      System.out.println("Please enter the secret phrase:");
+      String phrase = scanner.nextLine();
+      if(p.validateSecretPhrase(phrase)){
+//        valid = true;
+        System.out.println("Welcome back agent " + p.getServiceNumber());
+      } else{
+        p.setBlackList(true);
+        System.out.println("You have been blacklisted, agent " + p.getServiceNumber());
+      }
+//    } while (scanner.hasNextLine());
+
+
   }
 
-  public static boolean isNumeric(String str)
-  {
-    for (char c : str.toCharArray())
-    {
-      if (!Character.isDigit(c)) return false;
-    }
-    return true;
-  }
+
 }
